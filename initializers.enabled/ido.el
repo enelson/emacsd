c5c2822b0e8c7d40b8b7e91e254bd42295f965dc
Eric Nelson <eric@energisticsoftware.com>
2015-07-27 12:55:32 -0600

    changes

--- initializers.enabled/ido.el
+++ initializers.enabled/ido.el
@@ -3,3 +3,10 @@
 (ido-vertical-mode 1)
 
 (load-file "~/.emacs.d/vendor/ido-other-window/ido-other-window.el")
+
+;; (add-hook 'ido-setup-hook 'ido-my-keys)
+;;
+;;(defun ido-my-keys ()
+;;  "Add my keybindings for ido."
+;;  (define-key ido-completion-map " " 'ido-next-match)
+;;  )
